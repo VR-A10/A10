b3cb027204facd756c26c23c88461aa747a71a3f
YosukeOnuma <yosukeyosuke2002@gmail.com>
2023-02-11 18:11:10 +0900

    Constriction improved
    also:
    bullet target changed from the cubes to the 3d models

--- A10/Assets/UdonSharp/bullet.cs
+++ A10/Assets/UdonSharp/bullet.cs
@@ -19,14 +19,14 @@ public class bullet : UdonSharpBehaviour
 
         }
 
-        if (other.name == "Visibility")
+        if (other.name == "Left Vision" || other.name == "Right Vision")
         {
-            other.gameObject.GetComponent<Renderer>().material.color = Color.red;
+            //other.gameObject.GetComponent<Renderer>().material.color = Color.red;
             visionBlock.GetComponent<MeshRenderer>().enabled = !visionBlock.GetComponent<MeshRenderer>().enabled;
         }
-        else if (other.name == "Hand")
+        else if (other.name == "Left Hand" || other.name == "Right Hand")
         {
-            other.gameObject.GetComponent<Renderer>().material.color = Color.red;
+            //other.gameObject.GetComponent<Renderer>().material.color = Color.red;
             handgun.GetComponent<gun>().shakeToggle();
 
 
