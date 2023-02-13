dc8536e30b5bf7553696e63e112504275cf40a8f
YosukeOnuma <yosukeyosuke2002@gmail.com>
2023-02-13 14:57:13 +0900

    damage animation

---
+++ A10/Assets/UdonSharp/damage.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class damage : UdonSharpBehaviour
+{
+    public GameObject damageplane;
+    private Animator anim;
+
+    void Start()
+    {
+        damageplane.SetActive(false);
+        anim = damageplane.GetComponent<Animator>();
+    }
+
+    void playDamage()
+    {
+        damageplane.SetActive(true);
+        //anim.Play();
+    }
+}
