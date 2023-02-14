d598253150ccbf57509e80820c68dd99a05ee949
YosukeOnuma <yosukeyosuke2002@gmail.com>
2023-02-15 01:01:17 +0900

    Damaged Effect
    Damaged Effect introduced

--- A10/Assets/UdonSharp/bullet.cs
+++ A10/Assets/UdonSharp/bullet.cs
@@ -7,33 +7,34 @@ using VRC.Udon;
 public class bullet : UdonSharpBehaviour
 {
     public GameObject handgun;
+    public GameObject damagePlane;
 
     int damage_amount = 10;
+
+
 
 
     private void OnParticleCollision(GameObject other)
     {
 
-        if (other.name != "Plane")
-        {
-            Debug.Log(other.name);
-
-        }
 
         if (other.name == "Left Vision" || other.name == "Right Vision")
         {
-            //other.gameObject.GetComponent<Renderer>().material.color = Color.red;
             GameObject visionBlock = other.gameObject.transform.parent.Find("Constriction").gameObject;
             visionBlock.GetComponent<MeshRenderer>().enabled = !visionBlock.GetComponent<MeshRenderer>().enabled;
+
         }
         else if (other.name == "Left Hand" || other.name == "Right Hand")
         {
             //other.gameObject.GetComponent<Renderer>().material.color = Color.red;
+            damagePlane.GetComponent<Animator>().SetTrigger("damaged");
             handgun.GetComponent<gun>().shakeToggle();
         }
         else if (other.name == "A10")
         {
             other.gameObject.GetComponent<a10>().A10hit(damage_amount);
+            damagePlane.GetComponent<Animator>().SetTrigger("damaged");
+
         }
 
 
